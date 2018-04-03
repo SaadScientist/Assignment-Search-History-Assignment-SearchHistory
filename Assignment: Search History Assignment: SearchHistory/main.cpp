a2b3f0e895c71eb9643d33b3aedd8bcb947fa656
Saad Arshad <muhammadsaad@muhammads-macbook-pro.local>
2018-04-02 22:06:07 -0700

    Initial Commit

---
+++ Assignment: Search History Assignment: SearchHistory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment: Search History Assignment: SearchHistory
+//
+//  Created by Muhammad Saad on 4/2/18.
+//  Copyright © 2018 Muhammad Saad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
